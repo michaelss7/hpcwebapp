f5159f1ea2570a36fff314eb0f6b9495962faedc
michaelss7 <128793389+michaelss7@users.noreply.github.com>
2023-10-09 15:33:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 from flask import Flask, request, jsonify
-import os, uuid
-from azure.identity import DefaultAzureCredential
-from azure.storage.queue import QueueServiceClient, QueueClient, QueueMessage
-
 
 app = Flask(__name__)
 
@@ -18,28 +14,7 @@ def index():
             dest = request.form.get('dest')
 
             # Do something with the data (e.g., send it to a webhook or store it in a database)
-    print("Azure Queue storage - Python quickstart sample")
-        # Create a unique name for the queue
-    #queue_name = "quickstartqueues-" + str(uuid.uuid4())
-    queue_name = "testqueue"
-
-    account_url = "https://saweuprparis.queue.core.windows.net"
-    default_credential = DefaultAzureCredential()
-
-    # Create the QueueClient object
-    # We'll use this object to create and interact with the queue
-    queue_client = QueueClient(account_url, queue_name=queue_name ,credential=default_credential)
-
-    print("\nAdding messages to the queue...")
-
-    # Send several messages to the queue
-    queue_client.send_message(u"TEST MESSAGE")
-
-except Exception as ex:
-    print('Exception:')
-    print(ex)
-
-
+    
             # Respond with a success message
             return jsonify({"message": "Data received and processed successfully"})
 
