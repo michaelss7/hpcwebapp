ce98e3d60a6957ab0316bc2f36cb8c968959645c
michaelss7 <128793389+michaelss7@users.noreply.github.com>
2023-10-09 16:45:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def index():
             jobid = request.form.get('jobid')
             apikey = request.form.get('apikey')
             dest = request.form.get('dest')
+            ss7email = request.form.get('ss7email')
             
 
             # Do something with the data (e.g., send it to a webhook or store it in a database)
@@ -23,11 +24,11 @@ def index():
             account_url = "https://saweuprparis.queue.core.windows.net"
             default_credential = DefaultAzureCredential()
             queue_client = QueueClient(account_url, queue_name=queue_name ,credential=default_credential)
-            data = jobid + ";" + apikey + ";" + dest
+            data = jobid + ";" + apikey + ";" + dest + ";" + ss7email
             queue_client.send_message(data)
 
             # Respond with a success message
-            return jsonify({"message": "Data received and processed successfully"})
+            return jsonify({"message": "Data received and sent to queue. You will be notified by email when data copy is complete"})
 
         except Exception as e:
             return jsonify({"error": str(e)}), 500  # Respond with an error if something goes wrong
@@ -48,8 +49,11 @@ def index():
                 <label for="apikey">APIKey:</label>
                 <input type="text" id="apikey" name="apikey" required placeholder="Rescale API Key"><br><br>
 
+                <label for="ss7email">Subsea7 email:</label>
+                <input type="text" id="ss7email" name="ss7email" required placeholder="john.smith@subsea7.com"><br><br>
+
                 <label for="dest">Destination Path:</label>
-                <input type="text" id="dest" name="dest" required placeholder="\\subsea7.net\hpcdata\data\pathtodata"><br><br>
+                <input type="text" id="dest" name="dest" required size="40" placeholder="\\\\subsea7.net\hpcdata\pathtodata"><br><br>
 
                 <input type="submit" value="Submit">
             </form>
