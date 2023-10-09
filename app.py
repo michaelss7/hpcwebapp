214801d9ffac8764c558f9b07b1a81d092bc43c8
michaelss7 <128793389+michaelss7@users.noreply.github.com>
2023-10-09 16:07:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import os, uuid
 from flask import Flask, request, jsonify
+from azure.identity import DefaultAzureCredential
+from azure.storage.queue import QueueServiceClient, QueueClient, QueueMessage
 
 app = Flask(__name__)
 
@@ -12,9 +15,17 @@ def index():
             jobid = request.form.get('jobid')
             apikey = request.form.get('apikey')
             dest = request.form.get('dest')
+            
 
             # Do something with the data (e.g., send it to a webhook or store it in a database)
-    
+            
+            queue_name = "testqueue"
+            account_url = "https://saweuprparis.queue.core.windows.net"
+            default_credential = DefaultAzureCredential()
+            queue_client = QueueClient(account_url, queue_name=queue_name ,credential=default_credential)
+            data = jobid + ";" + apikey + ";" + dest
+            queue_client.send_message(data)
+
             # Respond with a success message
             return jsonify({"message": "Data received and processed successfully"})
 
